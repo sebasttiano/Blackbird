b1f9c739c1b078a44a07044a310f4fbadcca0fde
Sergey Voronov <voronov.s@selectel.ru>
2023-12-13 23:06:31 +0300

    Final version. All tests passed

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -24,8 +24,6 @@ func NewMemStorage() MemStorage {
 	}
 }
 
-type Middleware func(http.Handler) http.Handler
-
 // middleware gets Handler, makes some validation and returns also Handler.
 func middleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
@@ -38,81 +36,47 @@ func middleware(next http.Handler) http.Handler {
 	})
 }
 
-func Conveyor(h http.Handler, middlewares ...Middleware) http.Handler {
-	for _, middleware := range middlewares {
-		h = middleware(h)
-	}
-	return h
-}
-
-func mainPage(res http.ResponseWriter, req *http.Request) {
-	body := fmt.Sprintf("Method: %s\r\n", req.Method)
-	body += "Header ===============\r\n"
-	for k, v := range req.Header {
-		body += fmt.Sprintf("%s: %v\r\n", k, v)
-	}
-	body += "Query parameters ===============\r\n"
-	for k, v := range req.URL.Query() {
-		body += fmt.Sprintf("%s: %v\r\n", k, v)
-	}
-	res.Write([]byte(body))
-}
-
-func updateGauge(res http.ResponseWriter, req *http.Request) {
+// updateMetric handles update metrics request
+func updateMetric(res http.ResponseWriter, req *http.Request) {
 
 	incomingParams := strings.Split(req.URL.Path, "/")
-	if len(incomingParams) != 2 {
+	if len(incomingParams) != 3 {
 		res.WriteHeader(http.StatusNotFound)
 		return
 	}
 
-	metricName := incomingParams[0]
-	valueFloat, err := strconv.ParseFloat(incomingParams[1], 64)
-	if err != nil {
+	switch incomingParams[0] {
+	case "gauge":
+		metricName := incomingParams[1]
+		valueFloat, err := strconv.ParseFloat(incomingParams[2], 64)
+		if err != nil {
+			res.WriteHeader(http.StatusBadRequest)
+			io.WriteString(res, fmt.Sprintf("ERROR: %v\n", err))
+			return
+		}
+		localStorage.gauge[metricName] = valueFloat
+		res.Header().Set("Content-type", "text/plain")
+		res.WriteHeader(http.StatusOK)
+		io.WriteString(res, fmt.Sprintf("%f\n", localStorage.gauge[metricName]))
+	case "counter":
+		metricName := incomingParams[1]
+		valueInt, err := strconv.ParseInt(incomingParams[2], 10, 64)
+		if err != nil {
+			res.WriteHeader(http.StatusBadRequest)
+			io.WriteString(res, fmt.Sprintf("ERROR: %v\n", err))
+			return
+		}
+		localStorage.counter[metricName] += valueInt
+		res.Header().Set("Content-type", "text/plain")
+		res.WriteHeader(http.StatusOK)
+		io.WriteString(res, fmt.Sprintf("%d\n", localStorage.counter[metricName]))
+	default:
 		res.WriteHeader(http.StatusBadRequest)
-		io.WriteString(res, fmt.Sprintf("ERROR: %v\n", err))
+		io.WriteString(res, fmt.Sprintf("ERROR: UNKNOWN METRIC TYPE. Only gauge and counter are available\n"))
 		return
 	}
 
-	localStorage.gauge[metricName] = valueFloat
-	res.Header().Set("Content-type", "text/plain")
-	res.WriteHeader(http.StatusOK)
-	io.WriteString(res, fmt.Sprintf("%f\n", localStorage.gauge[metricName]))
 }
-
-func updateCounter(res http.ResponseWriter, req *http.Request) {
-
-	incomingParams := strings.Split(req.URL.Path, "/")
-	if len(incomingParams) != 2 {
-		res.WriteHeader(http.StatusNotFound)
-		return
-	}
-
-	metricName := incomingParams[0]
-	valueInt, err := strconv.ParseInt(incomingParams[1], 10, 64)
-	if err != nil {
-		res.WriteHeader(http.StatusBadRequest)
-		io.WriteString(res, fmt.Sprintf("ERROR: %v\n", err))
-		return
-	}
-
-	localStorage.counter[metricName] += valueInt
-	res.Header().Set("Content-type", "text/plain")
-	res.WriteHeader(http.StatusOK)
-	io.WriteString(res, fmt.Sprintf("%d\n", localStorage.counter[metricName]))
-}
-
-func updatePage(res http.ResponseWriter, req *http.Request) {
-	var body string
-
-	body += "ANOTHER SECTION ===============\r\n"
-	for k, v := range req.URL.Query() {
-		body += fmt.Sprintf("%s: %v\r\n", k, v)
-	}
-
-	res.Write([]byte(body))
-}
-
 func main() {
 	if err := run(); err != nil {
 		panic(err)
@@ -122,10 +86,7 @@ func main() {
 
 func run() error {
 	mux := http.NewServeMux()
-	mux.Handle("/", middleware(http.HandlerFunc(mainPage)))
-	mux.Handle("/update/", middleware(http.HandlerFunc(updatePage)))
-	mux.Handle("/update/gauge/", http.StripPrefix("/update/gauge/", middleware(http.HandlerFunc(updateGauge))))
-	mux.Handle("/update/counter/", http.StripPrefix("/update/counter/", middleware(http.HandlerFunc(updateCounter))))
+	mux.Handle("/update/", http.StripPrefix("/update/", middleware(http.HandlerFunc(updateMetric))))
 
 	err := http.ListenAndServe(`localhost:8080`, mux)
 	return err
