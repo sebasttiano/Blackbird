98031c2427e7e93e579000036f41e1c387dec321
Sergey Voronov <voronov.s@selectel.ru>
2023-12-13 16:18:11 +0300

    Working version, before autotest

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,15 +2,26 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
-var localStorage MemStorage
+var localStorage MemStorage = NewMemStorage()
 
 // MemStorage Keeps gauge and counter
 type MemStorage struct {
 	gauge   map[string]float64
 	counter map[string]int64
+}
+
+// NewMemStorage — constructor of the type MemStorage.
+func NewMemStorage() MemStorage {
+	return MemStorage{
+		gauge:   make(map[string]float64),
+		counter: make(map[string]int64),
+	}
 }
 
 type Middleware func(http.Handler) http.Handler
@@ -48,11 +59,47 @@ func mainPage(res http.ResponseWriter, req *http.Request) {
 }
 
 func updateGauge(res http.ResponseWriter, req *http.Request) {
-	res.Write([]byte(req.URL.Path + "\n"))
+
+	incomingParams := strings.Split(req.URL.Path, "/")
+	if len(incomingParams) != 2 {
+		res.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	metricName := incomingParams[0]
+	valueFloat, err := strconv.ParseFloat(incomingParams[1], 64)
+	if err != nil {
+		res.WriteHeader(http.StatusBadRequest)
+		io.WriteString(res, fmt.Sprintf("ERROR: %v\n", err))
+		return
+	}
+
+	localStorage.gauge[metricName] = valueFloat
+	res.Header().Set("Content-type", "text/plain")
+	res.WriteHeader(http.StatusOK)
+	io.WriteString(res, fmt.Sprintf("%f\n", localStorage.gauge[metricName]))
 }
 
 func updateCounter(res http.ResponseWriter, req *http.Request) {
-	res.Write([]byte(req.URL.Path + "\n"))
+
+	incomingParams := strings.Split(req.URL.Path, "/")
+	if len(incomingParams) != 2 {
+		res.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	metricName := incomingParams[0]
+	valueInt, err := strconv.ParseInt(incomingParams[1], 10, 64)
+	if err != nil {
+		res.WriteHeader(http.StatusBadRequest)
+		io.WriteString(res, fmt.Sprintf("ERROR: %v\n", err))
+		return
+	}
+
+	localStorage.counter[metricName] += valueInt
+	res.Header().Set("Content-type", "text/plain")
+	res.WriteHeader(http.StatusOK)
+	io.WriteString(res, fmt.Sprintf("%d\n", localStorage.counter[metricName]))
 }
 
 func updatePage(res http.ResponseWriter, req *http.Request) {
@@ -77,8 +124,8 @@ func run() error {
 	mux := http.NewServeMux()
 	mux.Handle("/", middleware(http.HandlerFunc(mainPage)))
 	mux.Handle("/update/", middleware(http.HandlerFunc(updatePage)))
-	mux.Handle("/update/gauge/", middleware(http.HandlerFunc(updateGauge)))
-	mux.Handle("/update/counter/", middleware(http.HandlerFunc(updateCounter)))
+	mux.Handle("/update/gauge/", http.StripPrefix("/update/gauge/", middleware(http.HandlerFunc(updateGauge))))
+	mux.Handle("/update/counter/", http.StripPrefix("/update/counter/", middleware(http.HandlerFunc(updateCounter))))
 
 	err := http.ListenAndServe(`localhost:8080`, mux)
 	return err
