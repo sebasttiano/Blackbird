c2b2043e5b769ec2cfdc0479a94da0995c55f4e1
Sergey Voronov <voronov.s@selectel.ru>
2023-12-20 19:29:58 +0300

    Pass iter5 tests

--- cmd/server/flags.go
+++ cmd/server/flags.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"os"
 )
 
 // non-export var flagRunAddr keeps ip address and port to run server on
@@ -11,4 +12,8 @@ var flagRunAddr string
 func parseFlags() {
 	flag.StringVar(&flagRunAddr, "a", "localhost:8080", "address and port to run server")
 	flag.Parse()
+
+	if envRunAddr := os.Getenv("ADDRESS"); envRunAddr != "" {
+		flagRunAddr = envRunAddr
+	}
 }
