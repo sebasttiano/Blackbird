71551cea9103e9f22f4f18c54730400594985d5e
Sergey Voronov <voronov.s@selectel.ru>
2024-04-15 22:00:00 +0300

    Refactor agent bench

--- internal/agent/agent_test.go
+++ internal/agent/agent_test.go
@@ -8,34 +8,30 @@ import (
 
 func BenchmarkAgentMetrics(b *testing.B) {
 	a := NewAgent("localhost:8080", 1, 1, "")
-	ctx, cancel := context.WithTimeout(context.TODO(), 3*time.Second)
-	defer cancel()
-
-	jobsMetric := make(chan MetricsSet)
-	jobsGMetrics := make(chan GopsutilMetricsSet)
 
 	var jobsMetricCount int
 	var jobsGMetricCount int
 
 	b.Run("BenchmarkGetMetrics", func(b *testing.B) {
 		for i := 0; i < b.N; i++ {
+			ctx, cancel := context.WithTimeout(context.TODO(), time.Duration(100*b.N)*time.Millisecond)
+			jobsMetric := make(chan MetricsSet, 50)
 			a.WG.Add(1)
-			go a.GetMetrics(ctx, 1*time.Second, jobsMetric)
-			for range jobsMetric {
-				jobsMetricCount++
-			}
+			a.GetMetrics(ctx, 10*time.Millisecond, jobsMetric)
+			jobsMetricCount = +len(jobsMetric)
+			cancel()
 		}
 		b.ReportMetric(float64(jobsMetricCount)/float64(b.N), "metric_jobs/op")
 	})
 	b.Run("BenchmarkGetGopsutilMetrics", func(b *testing.B) {
 		for i := 0; i < b.N; i++ {
+			ctx, cancel := context.WithTimeout(context.TODO(), time.Duration(100*b.N)*time.Millisecond)
+			jobsGMetrics := make(chan GopsutilMetricsSet, 100)
 			a.WG.Add(1)
-			go a.GetGopsutilMetrics(ctx, 1*time.Second, jobsGMetrics)
-			for range jobsGMetrics {
-				jobsGMetricCount++
-			}
+			a.GetGopsutilMetrics(ctx, 10*time.Millisecond, jobsGMetrics)
+			jobsGMetricCount += len(jobsGMetrics)
+			cancel()
 		}
 		b.ReportMetric(float64(jobsGMetricCount)/float64(b.N), "gopsutil_jobs/op")
 	})
-	a.WG.Wait()
 }
